e08a481a03cd7df47421fce4dd5db5f317af0912
Nathaniel Charles <oepnoepn@gmail.com>
2016-01-02 19:36:59 -0500

    Build modernizr

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,9 @@ var autoprefixer = require('autoprefixer');
 var browserSync = require('browser-sync');
 var csswring = require('csswring');
 var del = require('del');
+var modernizr = require('modernizr');
 var path = require('path');
+var source = require('vinyl-source-stream');
 
 // Config
 // - - - - - - - - - - - - - - - -
@@ -42,10 +44,9 @@ gulp.task('clean', function() {
 
 gulp.task('copy-assets', ['clean'], function() {
     return gulp.src([
-        paths.src + '/files/**/*.*',
-        paths.src + '/images/**/*.*',
-        paths.src + '/js/**/*.*'
-    ], {base: paths.src})
+            paths.src + '/files/**/*.*',
+            paths.src + '/images/**/*.*'
+        ], {base: paths.src})
         .pipe(gulp.dest(paths.public));
 });
 
@@ -62,6 +63,22 @@ var styles = function() {
 };
 gulp.task('styles', ['copy-assets'], styles);
 gulp.task('styles-watch', styles);
+
+gulp.task('scripts', function() {
+    modernizr.build({
+        minify: true,
+        options: [
+            'setClasses'
+        ],
+        'feature-detects': [
+            'test/css/flexbox'
+        ]
+    }, function(result) {
+        var stream = source('modernizr.min.js');
+        stream.end(result);
+        stream.pipe(gulp.dest(paths.public + '/js'));
+    });
+});
 
 gulp.task('browser-sync', function() {
     browserSync({
@@ -87,5 +104,5 @@ gulp.task('watch', ['browser-sync'], function() {
     });
 });
 
-gulp.task('build', ['styles']);
+gulp.task('build', ['styles', 'scripts']);
 gulp.task('default', ['build']);
